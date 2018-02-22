30086c56885d33f284f32d56d9203a10102dabcc
Felipe Flamarion da Conceicao <felipefc@itflex.com.br>
2018-02-22 17:40:31 -0300

    Modificações no layout

--- fifazzo/src/App.vue
+++ fifazzo/src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header>
+      Welcome to <br />
+      <span>FIFAZZO</span>
+    </header>
+
+    <div id="authentication">
+      <form method="POST" action=".">
+        <input type="text" placeholder="Username"/>
+        <input type="password" placeholder="Password"/>
+        <input type="submit" value="SIGN IN"/>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -17,7 +27,50 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #147380;
   margin-top: 60px;
+}
+
+#authentication {
+  margin: 50px;
+}
+
+#authentication form input {
+  width: 50%;
+  margin-top: 5px;
+  height: 30px;
+  font-size: 15px;
+  text-align: center;
+  border-radius: 3px;
+  border: 1px solid #4e946d;
+}
+
+#authentication form input[type="submit"] {
+  background: #4e946d;
+  height: 40px;
+  color: #fff;
+}
+
+#authentication form input[type="submit"]:hover {
+  background: #57b380;
+  height: 40px;
+  color: #fff;
+  transition: 200ms;
+  cursor: pointer;
+}
+
+header {
+  font-size: 50px;
+}
+
+header span{
+  font-size: 100px;
+  color: #213133;
+  font-weight: bold;
+}
+
+.block {
+  color: #147380;
+  margin: 25px;
 }
 </style>
